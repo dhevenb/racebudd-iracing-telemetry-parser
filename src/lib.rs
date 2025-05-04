49d04194638f76b4e26744ed3741e9bd3a4f2c09
Dheven <dheven.spam@gmail.com>
2025-05-04 12:21:50 -0400

    refactor: remove old get_lap_data function

--- src/lib.rs
+++ src/lib.rs
@@ -39,32 +39,6 @@ impl<'a> RaceBuddTelem {
 
     // Returns all lap data for the given laps
     pub fn get_lap_data(&mut self, laps: &Vec<i32>) -> HashMap<i32, Vec<Tick>> {
-        let mut lap_data: HashMap<i32, Vec<Tick>> = HashMap::new();
-        let lap_indices: HashMap<i32, i32> = self.lap_indices();
-        let mut telemetry_data = self.telemetry_data();
-
-        // Iterate through each target lap
-        for lap in laps {
-            let mut data: Vec<Tick> = Vec::new();
-            let lap_first_index = lap_indices[lap];
-            let lap_last_index = lap_indices[&(lap + 1)] - 1;
-
-            // Jump straight to target lap first index
-            if let Some(tick) = telemetry_data.nth(lap_first_index as usize) {
-                for _ in lap_first_index..=lap_last_index {
-                    if let Some(tick) = telemetry_data.next() {
-                        data.push(tick);
-                    }
-                }
-            }
-
-            lap_data.insert(*lap, data);
-        }
-        lap_data
-    }
-
-    // Returns all lap data for the given laps
-    pub fn try_different_way(&mut self, laps: &Vec<i32>) -> HashMap<i32, Vec<Tick>> {
         let lap_var = self.available_vars().iter().find(|var| var.name == "Lap").cloned().unwrap();
         let mut lap_data: HashMap<i32, Vec<Tick>> = HashMap::new();
         let mut telemetry_data: Ticks<'_> = self.telemetry_data();
--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,10 @@ pub fn main() {
     let file_name: &str = r"C:\Users\Dheven\Documents\iRacing\telemetry\mercedesamggt4_brandshatch grandprix 2025-05-02 20-05-46.ibt";
     let mut telem = RaceBuddTelem::new(file_name);
 
-    let target_laps = vec![1,2,3,4,5];
+    let target_laps = vec![1,2,3,4,5,6,7,8];
+    let test1 = Instant::now();
+    let lap_data = telem.get_lap_data(&target_laps);
 
-    {
-        let test1 = Instant::now();
-        let lap_data = telem.get_lap_data(&target_laps);
-        println!("Duration: {:?}", test1.elapsed());
-    }
-
-    {
-        let test2 = Instant::now();
-        let lap_data = telem.try_different_way(&target_laps);
-        println!("Duration: {:?}", test2.elapsed());
-    }
-    
-    /*
-    Benchmark test between the above implementations:
-    Starting...
-    Duration: 4.1990561s 
-    Duration: 1.2646498s
-    */
-
-    /*
     for lap in lap_data {
         for tick in lap.1 {
             println!("Throttle: {:?}", tick.throttle().float_32());
@@ -37,7 +19,9 @@ pub fn main() {
             println!("ABS Active: {:?}", tick.brake_abs_active().bool());
         }
     }
-    */
+
+    println!("Duration: {:?}", test1.elapsed());
+
 }
 
 /*
